dc9cea15f1d5aba6847cb7d70f9ab661be762d5d
juan <juand_david2001@hotmail.com>
2024-02-18 15:01:18 -0600

    add move to pacman

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, Injectable, OnInit } from '@angular/core';
 import { AudioComponent } from './audio/audio.component';
-import { timer } from 'rxjs';
+import { interval, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,35 +8,110 @@ import { timer } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent{
+export class AppComponent implements OnInit{
 
   constructor(private audio: AudioComponent){}
 
   title = 'pac-man';
 
+  //------PACMAN------
   up=1;
   trying=["1", "2", "3"];
   score=0;
+  position=0;
+  pacmanPosition:any={
+    x:0,
+    y:0
+  }
+  intervalo:any
 
+  //-----GAME-----
   playing=false;
   sound=true;
+  frameSize:any={
+    width:0,
+    height:0,
+    top:0,
+    left:0
+  }
 
-  position=0;
+  ngOnInit(): void {
+    var frameData=document.getElementById('frame')?.getBoundingClientRect()
+    this.frameSize.width=frameData?.width
+    this.frameSize.hight=frameData?.height
+    this.frameSize.left=frameData?.left
+    this.frameSize.top=frameData?.top
+    this.pacmanPosition.x=frameData?.x
+    this.pacmanPosition.y=frameData?.y
+  }
 
-  pacman=document.addEventListener('keydown', (e)=>{
+  key=document.addEventListener('keydown', (e)=>{
     if(this.playing){
-      if(e.keyCode==37){
+      if(e.keyCode==37 && this.position!=1){
         this.position=1;
-      }else if(e.keyCode==38){
+        if(this.intervalo){
+          this.intervalo.unsubscribe()
+        }
+        this.intervalo=interval(10).subscribe(()=>{
+          this.pacmanPosition.x=this.moverIzquiera(this.pacmanPosition.x);
+        })
+        
+      }else if(e.keyCode==38 && this.position!=2){
         this.position=2;
-      }else if(e.keyCode==39){
+        if(this.intervalo){
+          this.intervalo.unsubscribe()
+        }
+        this.intervalo=interval(10).subscribe(()=>{
+          this.pacmanPosition.y=this.moverUp(this.pacmanPosition.y);
+        })
+      }else if(e.keyCode==39 && this.position!=3){
         this.position=3;
-      }else if(e.keyCode==40){
+        if(this.intervalo){
+          this.intervalo.unsubscribe()
+        }
+        this.intervalo=interval(10).subscribe(()=>{
+          this.pacmanPosition.x=this.moverDerecha(this.pacmanPosition.x);
+        })
+      }else if(e.keyCode==40 && this.position!=4){
         this.position=4;
+        if(this.intervalo){
+          this.intervalo.unsubscribe()
+        }
+        this.intervalo=interval(10).subscribe(()=>{
+          this.pacmanPosition.y=this.moverBottom(this.pacmanPosition.y);
+        })
       }
     }
-    console.log(this.position);
   })
+
+  moverDerecha(position:any){
+    if(position<(this.frameSize.left+this.frameSize.width)-21){
+      position+=1
+    }
+    return position
+  }
+
+  moverIzquiera(position:any){
+    if(position>this.frameSize.left){
+      position-=1
+    }
+    return position
+  }
+
+  moverUp(position:any){
+    if(position>this.frameSize.top){
+      position-=1
+    }
+    return position
+  }
+
+  moverBottom(position:any){
+    if(position<(this.frameSize.top+212)-21){
+      position+=1
+      console.log("down")
+    }
+    return position
+  }
 
   start(){
     this.playing=false;
